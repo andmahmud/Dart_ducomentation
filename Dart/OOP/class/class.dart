11f5ea232b3a88b224bd99861d53606259b85a15
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2024-11-22 15:39:10 +0600

    Add files via upload

---
+++ Dart/OOP/class/class.dart
@@ -0,0 +1,8 @@
+import 'myclass.dart';
+
+void main() {
+  var obj = new myclass();
+  print(obj.name);
+  obj.AddTowNumber(40, 60);
+  obj.AddThreeNumber(10, 40, 70);
+}
---
+++ Dart/OOP/class/myclass.dart
@@ -0,0 +1,10 @@
+class myclass {
+  var name = 'Mahmud';
+  AddTowNumber(var x, var y) {
+    print(x + y);
+  }
+
+  AddThreeNumber(var x, var y, var z) {
+    print(x + y + z);
+  }
+}
